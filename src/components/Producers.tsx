599aef7a3077b5bdce652bd9ea54d01a6e99c2f0
se7t <se7t.contact@gmail.com>
2021-04-27 16:22:49 +0200

    feat(producers.tsx): add video playback on item hover

--- src/components/Producers.tsx
+++ src/components/Producers.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/media-has-caption */
 import React from 'react';
 import styled from '@emotion/styled';
 
@@ -37,6 +38,26 @@ const ProducersItem = styled(`div`)`
         z-index: 1;
         top: 0;
     }
+
+    video {
+        height: 100%;
+        opacity: 0;
+        position: absolute;
+        top: 0px;
+        width: 100%;
+        z-index: 0;
+    }
+
+    &:hover {
+        border-color: rgba(249, 249, 249, 0.8);
+        box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
+            rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+        transform: scaleX(1.05);
+        video {
+            opacity: 1;
+            transition: opacity 200ms ease-in 0s;
+        }
+    }
 `;
 
 const Producers: React.FC = () => {
@@ -44,21 +65,39 @@ const Producers: React.FC = () => {
         <ProducersList>
             <ProducersItem>
                 <img src="images/producers/disney.webp" alt="Disney" />
+                <video autoPlay loop playsInline>
+                    <source src="videos/disney.mp4" type="video/mp4" />
+                </video>
             </ProducersItem>
             <ProducersItem>
                 <img src="images/producers/marvel.webp" alt="Marvel" />
+                <video autoPlay loop playsInline>
+                    <source src="videos/marvel.mp4" type="video/mp4" />
+                </video>
             </ProducersItem>
             <ProducersItem>
                 <img
                     src="images/producers/national-geographic.webp"
                     alt="National Geographic"
                 />
+                <video autoPlay loop playsInline>
+                    <source
+                        src="videos/national-geographic.mp4"
+                        type="video/mp4"
+                    />
+                </video>
             </ProducersItem>
             <ProducersItem>
                 <img src="images/producers/pixar.webp" alt="Pixar" />
+                <video autoPlay loop playsInline>
+                    <source src="videos/pixar.mp4" type="video/mp4" />
+                </video>
             </ProducersItem>
             <ProducersItem>
                 <img src="images/producers/starwars.webp" alt="Star Wars" />
+                <video autoPlay loop playsInline>
+                    <source src="videos/star-wars.mp4" type="video/mp4" />
+                </video>
             </ProducersItem>
         </ProducersList>
     );
