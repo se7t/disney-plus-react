81df9322c3ce5498e5aeb42a7797538cb5e8f83d
se7t <se7t.contact@gmail.com>
2021-04-27 14:31:59 +0200

    feat(home.tsx): add background gradient

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
-import '@emotion/styled';
+import styled from '@emotion/styled';
+
+const Container = styled(`main`)`
+    position: relative;
+    min-height: calc(100vh - 250px);
+    overflow-x: hidden;
+    display: block;
+    top: 4.375rem;
+    padding: 0 calc(3.5vw + 5px);
+
+    &:after {
+        background: url('/images/home-background.webp') center center / cover
+            no-repeat fixed;
+        content: '';
+        position: absolute;
+        inset: 0px;
+        opacity: 1;
+        z-index: -1;
+    }
+`;
 
 const Home: React.FC = () => {
-    return <div>Home</div>;
+    return <Container>Hi</Container>;
 };
 
 export default Home;
