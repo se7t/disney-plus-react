8d049c2afbfc59ede33b1545ec328ff5e21c1032
se7t <se7t.contact@gmail.com>
2021-04-28 18:46:44 +0200

    feat(detailspage.tsx): add play & trailer buttons

--- src/components/DetailsPage.tsx
+++ src/components/DetailsPage.tsx
@@ -15,7 +15,7 @@ const Background = styled(`div`)`
     opacity: 0.8;
     position: fixed;
     right: 0px;
-    top: 0px;
+    top: 4.375rem;
     z-index: -1;
 
     img {
@@ -44,6 +44,66 @@ const ImageTitle = styled(`div`)`
     }
 `;
 
+const ContentMeta = styled(`div`)`
+    max-width: 874px;
+`;
+
+const Controls = styled(`div`)`
+    display: flex;
+    align-items: center;
+    flex-flow: row nowrap;
+    margin: 24px 0px;
+    min-height: 56px;
+`;
+
+const PlayButton = styled(`button`)`
+    display: flex;
+    font-size: 1rem;
+    margin: 0px 22px 0px 0px;
+    padding: 0px 24px;
+    height: 56px;
+    border-radius: 4px;
+    align-items: center;
+    cursor: pointer;
+    justify-content: center;
+    letter-spacing: 1.8px;
+    text-align: center;
+    text-transform: uppercase;
+    background: rgb(249, 249, 249);
+    border: none;
+    color: #040714;
+
+    img {
+        width: 2rem;
+    }
+
+    &:hover {
+        background: rgb(198, 198, 198);
+        transition: all 200ms ease 0s;
+    }
+
+    @media (max-width: 768px) {
+        height: 45px;
+        padding: 0px 12px;
+        font-size: 0.75rem;
+        margin: 0px 10px 0px 0px;
+
+        img {
+            width: 25px;
+        }
+    }
+`;
+
+const TrailerButton = styled(PlayButton)`
+    background: rgba(4, 7, 20, 0.3);
+    border: 1px solid rgb(249, 249, 249);
+    color: rgb(249, 249, 249);
+
+    &:hover {
+        background: rgba(4, 7, 20, 0.8);
+    }
+`;
+
 const DetailsPage: React.FC = () => {
     return (
         <Container>
@@ -59,6 +119,24 @@ const DetailsPage: React.FC = () => {
                     alt="Title"
                 />
             </ImageTitle>
+            <ContentMeta>
+                <Controls>
+                    <PlayButton>
+                        <img
+                            src="/images/icons/play-icon-black.webp"
+                            alt="Play icon"
+                        />
+                        <span>Play</span>
+                    </PlayButton>
+                    <TrailerButton>
+                        <img
+                            src="/images/icons/play-icon-white.webp"
+                            alt="Play icon"
+                        />
+                        <span>Trailer</span>
+                    </TrailerButton>
+                </Controls>
+            </ContentMeta>
         </Container>
     );
 };
